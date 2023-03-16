b87ad64f4474db3b8a34d2716c7cd5e7c6177be5
floxdeveloper <fritz66@alpenjodel.de>
2023-03-16 10:36:39 +0100

    Add name property

--- src/pdffile.ts
+++ src/pdffile.ts
@@ -14,11 +14,16 @@ export class PDFFile {
      * @public
      */
     content: ArrayBuffer;
+    /**
+     * @public
+     */
+    name: string;
 
     public static convertTFileToPDFFile(tFile: TFile, binaryContent: ArrayBuffer): PDFFile {
         let pdfFile = new PDFFile;
         pdfFile.extension = tFile.extension
         pdfFile.path = tFile.path
+        pdfFile.name = tFile.name
         pdfFile.content = binaryContent
         return pdfFile
     }
